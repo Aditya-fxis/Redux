bbd7f054795c490ead2c2f938c1d644f9977afee
Aditya-fxis <adityapatel@fxis.ai>
2025-01-09 14:16:55 +0530

    Add reset and increment by amount functionality to counter

--- ReduxCounter/src/App.jsx
+++ ReduxCounter/src/App.jsx
@@ -1,30 +1,57 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import {increment, decrement} from './feature/counterSlice.jsx'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement, reset, incrementByAmount } from "./feature/counterSlice.jsx";
 const App = () => {
-const count = useSelector((state)=> state.counter.value)
+  const count = useSelector((state) => state.counter.value);
 
-const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   function handleIncreament() {
     dispatch(increment());
   }
 
-  function handleDecreament(){
+  function handleDecreament() {
     dispatch(decrement());
   }
 
-  return (
-   <>
-   <div className='box'>
-   <div className='container'>
-      <button onClick={handleIncreament}>+</button>
-      <p>Counter: {count}</p>
-      <button onClick={handleDecreament}>-</button>      
-    </div>
-   </div>
-   </>
-  )
-}
+  function handleReset() {
+    dispatch(reset());
+  }
 
-export default App
+  function handleAmount() {
+    dispatch(incrementByAmount(amount));
+  }
+
+  const [amount, setAmount] = useState(0);
+  return (
+    <>
+      <div className="box">
+        <div className="container">
+
+          <div className="amount">
+            <input className="input" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter Amount"/>
+            <button className="abtn" onClick={handleAmount}>
+              Increment by Amount
+            </button>
+          </div>
+
+
+          <div className="rcontainer">
+            <button onClick={handleIncreament}>+</button>
+            <p className="text">Counter: {count} </p>
+            <button onClick={handleDecreament}>-</button>
+          </div>
+
+
+          <button className="reset" onClick={handleReset}>
+            Reset
+          </button>
+
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
